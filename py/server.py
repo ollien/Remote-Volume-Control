d5e3918b3e11b6b886070238352c11b488fdc123
Nick Krichevsky <njk828@gmail.com>
2014-03-20 21:20:01 -0400

    Fix math

--- py/server.py
+++ py/server.py
@@ -4,11 +4,10 @@ import subprocess
 sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind(('192.168.1.55',5565))
 def getVolume():
-	#print subprocess.Popen("osascript -e 'output volume of (get volume settings)'",shell=True,stdout=subprocess.PIPE).communicate()[0]
-	print int(subprocess.Popen("osascript -e 'output volume of (get volume settings)'",shell=True,stdout=subprocess.PIPE).communicate()[0])
-	return int(subprocess.Popen("osascript -e 'output volume of (get volume settings)'",shell=True,stdout=subprocess.PIPE).communicate()[0])/8
+	#print subprocess.Popen("osascript -e 'output volume of (get volume settings)'",shell=True,stdout=subprocess.PIPE).communicate()[0]	
+	return int(subprocess.Popen("osascript -e 'output volume of (get volume settings)'",shell=True,stdout=subprocess.PIPE).communicate()[0])
 while True:
-	print str(getVolume()-5)
+	print str(getVolume())
 	data=sock.recvfrom(1024)
 	print data[0]
 	if (data[0]=='up' ):
@@ -16,10 +15,10 @@ while True:
 		print getVolume()
 		print "osascript -e 'set volume "+str(getVolume()+1)+"'"
 		if (getVolume()+1>7):
-			subprocess.Popen("osascript -e 'set volume "+str(getVolume()+2)+"'",shell=True)
+			subprocess.Popen("osascript -e 'set volume output volume "+str(getVolume()+10)+"'",shell=True)
 	elif (data[0]=='down'):
 		print getVolume()
 		print "osascript -e 'set volume "+str(getVolume()-1)+"'"
-		subprocess.Popen("osascript -e 'set volume "+str(getVolume()-1)+"'",shell=True)
+		subprocess.Popen("osascript -e 'set volume output volume "+str(getVolume()-10)+"'",shell=True)
 	elif (data[0]=='mute'):
 		subprocess.Popen("osascript -e 'set volume 0'",shell=True)
