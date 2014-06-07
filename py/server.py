5b1cff317f5fd7a4c912e7647d6b74b5ee580d6f
Nick Krichevsky <njk828@gmail.com>
2014-06-07 16:16:58 -0400

    Change ip to 0.0.0.0

--- py/server.py
+++ py/server.py
@@ -2,7 +2,7 @@ import socket
 import subprocess
 
 sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind(('192.168.1.55',5565))
+sock.bind(('0.0.0.0',5565))
 def getVolume():
 	#print subprocess.Popen("osascript -e 'output volume of (get volume settings)'",shell=True,stdout=subprocess.PIPE).communicate()[0]	
 	return int(subprocess.Popen("osascript -e 'output volume of (get volume settings)'",shell=True,stdout=subprocess.PIPE).communicate()[0])
